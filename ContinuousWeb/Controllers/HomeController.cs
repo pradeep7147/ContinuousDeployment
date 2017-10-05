03556e0fe7e992e54ddf62a9fee0c62ec8ddb5ea
PRADEEP RAO R <PRADEEP RAO R>
2017-10-05 15:09:15 +0530

    commited correct

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -15,10 +15,10 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult About()
         {
-            throw new NotImplementedException();
-            //ViewData["Message"] = "Your application description page.";
+            
+            ViewData["Message"] = "Your application description page.";
 
-            //return View();
+            return View();
         }
 
         public IActionResult Contact()
